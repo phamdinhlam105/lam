dae675636040fb833629384cb75f0eb9ee6167f1
Pham Dinh Lam <heartsstealer105@gmail.com>
2020-04-13 09:11:24 +0700

    Update GetThongTin.java

--- Bai1/src/bai1/GetThongTin.java
+++ Bai1/src/bai1/GetThongTin.java
@@ -5,10 +5,34 @@
  */
 package bai1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Ban Hanh Rong
  */
-public class GetThongTin implements I_thongtin {
+public class GetThongTin implements I_Thongtin {
+
+    @Override
+    public GetThongTin() {
+        Scanner sc = new Scanner(System.in);
+        Nhanvien nv=new Nhanvien();
+        System.out.println("Nhap Ten");
+         nv.Ten = sc.nextLine();
+         System.out.println("Nhap Tuoi");
+         nv.tuoi = sc.nextInt();
+         sc.nextLine();
+         System.out.println("Nhap DiaChi");
+         nv.DiaChi = sc.nextLine();
+         System.out.println("Nhap Luong");
+         nv.tienluong = sc.nextDouble();
+         System.out.println("Nhap T ongSo Gio Lam");
+         nv.tongsogiolam = sc.nextInt();
+    }
+
+    @Override
+    public double tinhThuong() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
